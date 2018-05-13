3c5688a0bc7312a7aadfb655b355d02d3d861f58
Lloyd <lloyd7miller@gmail.com>
2018-05-13 16:56:47 +1200

    first commit

---
+++ beginning.js
@@ -0,0 +1,36 @@
+document.write('\
+<header>\
+    <div class="header-item" onclick="location.href=\'#\'">\
+        <img src="img/msg.png" alt="messenger">\
+    </div>\
+    <div class="header-item" id="assignment-question">\
+        <div class="the-question" onclick="collapseExpand()">\
+            What is the role of lactic acid in physical exercise?\
+        </div>\
+        <!-- <button onclick="myFunction()" class="dropbtn">Dropdown</button> -->\
+        <div id="myDropdown" class="dropdown-content" style="visibility: hidden;">\
+            <h4>Assignment details</h4>\
+            <p id="due-date"><u><b>Due date:</b></u> May 30th 2018</p>\
+            <h5>Task sub-heading 1</h5>\
+            <p>\
+                Suspendisse sollicitudin finibus imperdiet. Quisque pretium pharetra nibh sit amet volutpat. Donec ullamcorper, dui fringilla\
+                ultricies lacinia, libero nulla lacinia nunc, at accumsan metus arcu eget ex. Donec lobortis commodo libero ut mollis. In\
+                ex dolor, viverra eu quam sit amet, tincidunt pharetra quam. Cras sagittis eros in mi gravida egestas. Aenean molestie dolor\
+                turpis, ac finibus mi feugiat consectetur. Nullam dapibus, urna gravida faucibus efficitur, purus mi sodales urna, at molestie\
+                velit nisi blandit tortor. Vivamus vulputate porta nibh, non semper ligula luctus a. Aliquam vitae laoreet tellus. Proin\
+                a luctus libero. \
+            </p>\
+            <h5>Task sub-heading 1</h5>\
+            <p>\
+                Integer blandit ac nisl sed luctus. Aliquam erat volutpat. Mauris at nisl porta, ullamcorper dolor eget,\
+                ullamcorper tellus. Nulla elit justo, volutpat a tincidunt vel, vestibulum vitae ligula. Phasellus nunc mauris, bibendum\
+                ut erat vitae, vulputate feugiat purus. Nullam eu leo sed neque congue egestas. Nunc pulvinar ut erat sit amet euismod. Nam\
+                dictum, tellus quis lacinia gravida, lacus massa suscipit nulla, eget imperdiet enim nisi eu mi. In pharetra efficitur lacinia.\
+                Integer eget risus in velit tincidunt pretium vitae id mauris.\
+            </p>\
+        </div>\
+    </div>\
+    \
+    <div class="header-item" onclick="location.href=\'#\'">Kody Johnson</div>\
+</header>\
+');
---
+++ main.js
@@ -0,0 +1,77 @@
+// ~~~ SOURCE: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onclick_dropdown
+// Get the button, and when the user clicks on it, execute myFunction
+var btn = document.getElementById("edit-btn")
+btn.onclick = function () { textEditor() };
+
+/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+function textEditor() {
+    var tinymce = document.getElementById("tinymce-div");
+    var ans = document.getElementsByClassName("your-answer")[0]
+    var warn = document.getElementById("warning");
+    var cancel = document.getElementById("cancel-btn");
+    if (tinymce.style.display === "none") {
+        tinymce.style.display = "block";
+        ans.style.display = "none";
+        warn.style.display = "block";
+        btn.innerHTML = "Save"
+        cancel.style.display = "block";
+    } else {
+        tinymce.style.display = "none";
+        ans.style.display = "block";
+        warn.style.display = "none";
+        btn.innerHTML = "Edit";
+        cancel.style.display = "none";
+    }
+}
+// ~~~ END SOURCE
+
+// SOURCE: https://stackoverflow.com/questions/23850115/trying-to-get-a-timestamp-inputed-upon-a-click-of-the-button
+function getTimeStamp() {
+    var now = new Date();
+    return ((now.getMonth() + 1) + '/' + (now.getDate()) + '/' + now.getFullYear() + " " + now.getHours() + ':'
+        + ((now.getMinutes() < 10) ? ("0" + now.getMinutes()) : (now.getMinutes())) + ':' + ((now.getSeconds() < 10) ? ("0" + now
+            .getSeconds()) : (now.getSeconds())));
+}
+// END SOURCE
+
+
+// SOURCE: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_js_dropdown
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function collapseExpand() {
+    var dropdown = document.getElementById("myDropdown");
+    if (dropdown.style.visibility === "hidden") {
+        dropdown.style.visibility = "visible";
+    }else{
+        dropdown.style.visibility = "hidden";
+    }
+        
+}
+
+//not working.....
+// var dropdown = document.getElementById("myDropdown");
+// dropdown.addEventListener("focusout", collapse);
+
+// function collapse() {
+//     dropdown.style.display = "none";
+//     alert("hello");
+// }
+
+// Close the dropdown if the user clicks outside of it
+// window.onclick = function (event) {
+//     if (!event.target.matches('.dropbtn')) {
+//         var dropdown = document.getElementsById("myDropdown");
+//         dropdown.style.display = "none";
+//     }
+// }
+
+// END SOURCE
+
+
+// SOURCE: https://stackoverflow.com/questions/4274489/how-can-i-make-an-upvote-downvote-button
+for (const btn of document.querySelectorAll('.vote')) {
+    btn.addEventListener('click', event => {
+        event.target.classList.toggle('on');
+    });
+}
+// END SOURCE
